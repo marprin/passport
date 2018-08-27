e95e56066db0108cd57ccc68bb2811a04cd97260
marprin <marprin.hennes@acommerce.asia>
2018-08-27 16:53:05 +0700

    Move config url and wsgi to config folder

--- passport/config/settings/base.py
+++ passport/config/settings/base.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'passport.urls'
+ROOT_URLCONF = 'passport.config.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'passport.wsgi.application'
+WSGI_APPLICATION = 'passport.config.wsgi.application'
 
 
 # Database
---
+++ passport/config/settings/local.py
@@ -0,0 +1,1 @@
+from passport.config.settings.base import *
