9539424cfb9e8009f7859a6ec5fd47cd5835b76a
marprin <marprin93@gmail.com>
2020-04-28 17:10:15 +0700

    Add more script

--- script.sh
+++ script.sh
@@ -4,4 +4,10 @@ if [ -z $1 ]; then
     echo "Please input paramters"
 elif [ $1 = 'create-migration' ]; then
     env $(cat .env | xargs) python3 manage.py makemigrations
+elif [ $1 = 'runserver' ]; then
+    env $(cat .env | xargs) python3 manage.py runserver
+elif [ $1 = 'migrate' ]; then
+    env $(cat .env | xargs) python3 manage.py migrate
+elif [ $1 = 'shell' ]; then
+    env $(cat .env | xargs) python3 manage.py shell
 fi
