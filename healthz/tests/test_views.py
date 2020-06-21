40c223caa995466382ae952ed567675ca94db3e8
marprin <marprin93@gmail.com>
2020-06-21 22:17:34 +0700

    Update healthz endpoint

--- healthz/tests/test_views.py
+++ healthz/tests/test_views.py
@@ -1,11 +1,12 @@
 from django.test import TestCase, Client
+from django.shortcuts import reverse
 from version import version
 
 
 class TestHealthz(TestCase):
     def setUp(self):
         self.client = Client()
-        self.healthz_url = "/healthz"
+        self.healthz_url = reverse("healthz")
         self.expected_response = {
             "version": version,
             "message": "ok",
